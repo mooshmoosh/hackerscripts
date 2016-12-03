5b5734d7f33f3628781cc53b108e2d8635fcff16
Will Mischlewski <will@mooshmoosh.me>
2016-12-03 13:45:07 +1100

    Added a function to gui scripts for choices
    There is now an extra function added to gui scripts to let you ask the
    user to select one of a list of options.

--- makezenityscript.py
+++ makezenityscript.py
@@ -4,23 +4,42 @@ import sys
 import os
 
 if len(sys.argv) < 3:
-    print("usage: " + sys.argv[0] + " {scriptname} [named parameters]")
+    print("usage: " + sys.argv[0] + " {scriptname} [--choices] [named parameters]")
     print("")
     print("    Creates a template bash script which uses zenity to create a dialog for")
     print("    the user to enter the parameters. This is useful for creating bash")
     print("    scripts that can be run by non technical people without them having to")
     print("    use the terminal.")
+    print("")
+    print("    if you supply the --choices option, then the user will be asked to")
+    print("    select one, from a list of the parameters in [named parameters]. The")
+    print("    user's choice will be in a variable called CHOICE.")
     exit()
 title = sys.argv[1]
 scriptName = title.lower().replace(' ', '_')
 parameters = sys.argv[2:]
 
-assignment_statements = []
-form_elements = []
-for (index, parameter) in enumerate(parameters):
-    form_elements.append('--add-entry="' + parameter + '"')
-    parameter_name = parameter.upper().replace(' ', '_')
-    assignment_statements.append(parameter_name + '="${PARAMETER_ARRAY[' + str(index) + ']}"')
+if parameters[0] == '--choices':
+    assignment_statements = []
+    parameter_parsing_code = 'CHOICE=$(zenity --list --text="{title}" --column="Option" "{options}")'.format(
+        title=title,
+        options='" "'.join(parameters[1:])
+    )
+else:
+    assignment_statements = ["# These are the parameters from the user"]
+    form_elements = []
+    for (index, parameter) in enumerate(parameters):
+        form_elements.append('--add-entry="' + parameter + '"')
+        parameter_name = parameter.upper().replace(' ', '_')
+        assignment_statements.append(parameter_name + '="${PARAMETER_ARRAY[' + str(index) + ']}"')
+
+    parameter_parsing_code = """
+    PARAMETERS=$(zenity --forms --text="{title}" {form_elements}) || exit
+    IFS="|" read -r -a PARAMETER_ARRAY <<< "$PARAMETERS"
+    """.format(
+        form_elements=' '.join(form_elements),
+        title=title
+    )
 
 resultingScript = """#!/bin/bash
 
@@ -38,21 +57,25 @@ alert () {{
     zenity --info --text="$1"
 }}
 
-PARAMETERS=$(zenity --forms --text="{title}" {form_elements}) || exit
-IFS="|" read -r -a PARAMETER_ARRAY <<< "$PARAMETERS"
+choose () {{
+    # Present the user with multiple choice use it like this:
+    #     CHOICE=$(choose "Please select a country" "Australia" "Mexico" "China")
+    TITLE=$1
+    shift
+    zenity --list --text="$TITLE" --column="Option" "$@"
+}}
 
+{parameter_parsing_code}
 # The current date/time is a common thing to need in scripts
 DATE_STAMP=`date +%%Y-%%m-%%d-%%H-%%M-%%S`
-# These are the parameters from the user
 {assignment_statements}
 
 # Put the main content of your script here:
 
 alert "All done!"
 """.format(
-    form_elements=' '.join(form_elements),
-    title=title,
-    assignment_statements="\n".join(assignment_statements)
+    assignment_statements="\n".join(assignment_statements),
+    parameter_parsing_code=parameter_parsing_code
 )
 
 scriptFilename = os.environ["HOME"] + "/bin/" + scriptName
