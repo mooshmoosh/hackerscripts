1fc6969f139a4724c75b795fff930dba2bf1673e
Will Mischlewski <developerwill1@gmail.com>
2017-12-15 17:44:09 +1100

    Added some revamping

--- c_stuff/lisp.c
+++ c_stuff/lisp.c
@@ -1,148 +1,214 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-#include <assert.h>
-#include <string.h>
 
-struct _LispPair;
-struct _LispPair {
-    void * data;
-    struct _LispPair * next;
+typedef enum {
+    LISPPAIR_STRING,
+    LISPPAIR_QUOTED_SYMBOL,
+    LISPPAIR_SYMBOL,
+    LISPPAIR_QUOTED_LIST,
+    LISPPAIR_LIST,
+    LISPPAIR_INT,
+    LISPPAIR_DOUBLE
+} LispPairType;
+
+struct _DepthFirstLispPair
+{
+    // This is the depth first search orderd version of the parsed tree.
+    // This is the order in which the symbols actually apear in the text
+    // being parsed. In an array of these structures, the integer next refers
+    // the the index of the next element in the list. The first element of
+    // a list is the element that comes after the list object.
+    LispPairType type;
+    unsigned int next;
+    union {
+        unsigned int as_str_offset;
+        long long int as_int;
+        double as_double;
+    } value;
+};
+typedef struct _DepthFirstLispPair * DepthFirstLispPair;
+
+struct _LispPair
+{
+    // This is the breadth first search ordered version of the parsed tree.
+    // This is what we return. In an array of these structures, the next
+    // one is the next element of a list. Lists are represented as pointers
+    // to the first element.
+    LispPairType type;
+    unsigned int length;
+    union {
+        const char * as_str;
+        const char * as_symbol;
+        struct LispPair * as_list;
+        long long int as_int;
+        double as_double;
+    } value;
 };
 typedef struct _LispPair * LispPair;
 
-struct _LispParser {
-    LispPair root;
-    LispPair parent;
-    LispPair strings;
-    LispPair * next_list;
-    enum {
-        LISP_PARSER_BETWEEN_ATOMS,
-        LISP_PARSER_IN_SYMBOL
-    } current_state;
-    char * buffer;
-    unsigned int buffer_size;
-    unsigned int symbol_length;
+struct _LispParser
+{
+    char * strings;
+    unsigned int strings_capacity;
+    unsigned int next_character;
+    DepthFirstLispPair parsed_tree;
+    LispPair result;
+    unsigned int parsed_tree_size;
+    int in_string;
+    int escaping;
+    int error;
     unsigned int line_number;
-    unsigned int character_number;
-    unsigned int error;
-    const char * error_message;
+    unsigned int column_number;
 };
 typedef struct _LispParser * LispParser;
 
-LispPair lisppair_new(void * data, LispPair next)
-{
-    LispPair result = calloc(1, sizeof(struct _LispPair));
-    assert((unsigned long long int) result % 2 == 0);
-    result->data = data;
-    result->next = next;
-    return result;
-}
-
 LispParser lispparser_new()
 {
-    LispParser result = calloc(1, sizeof(struct _LispParser));
-    result->root = NULL;
-    result->next_list = &result->root;
-    result->parent = NULL;
-    result->strings = NULL;
-    result->current_state = LISP_PARSER_BETWEEN_ATOMS;
-    result->buffer_size = 64;
-    result->buffer = calloc(result->buffer_size, 1);
-    result->symbol_length = 0;
-    result->line_number = 0;
-    result->character_number = 0;
-    return result;
+    LispParser parser = calloc(sizeof(struct _LispParser), 1);
+    parser->parsed_tree_capacity = 10;
+    parser->parsed_tree = calloc(sizeof(struct _DepthFirstLispPair), parser->parsed_tree_capacity);
+    parser->parsed_tree_size = 0;
+    parser->result = NULL;
+    parser->in_string = 0;
+    parser->escaping = 0;
+    parser->error = 0;
+    parser->line_number = 0;
+    parser->column_number = 0;
+    parser->strings_capacity = 256;
+    parser->strings = calloc(sizeof(char), parser->strings_capacity);
+    parser->next_character = 0;
+    return parser;
 }
 
-void * lispparser_create_object(LispParser parser, const char * symbol, unsigned int symbol_length)
+void lispparser_start_escaping(LispParser parser)
 {
-    LispPair string;
-    for (string = parser->strings; string != NULL; string = string->next) {
-        if (!strncmp((const char *) string->data, symbol, symbol_length)) {
-            return (void*) ((unsigned long long int)string->data | 1);
-        }
+    parser->escaping = 1;
+}
+
+void lispparser_close_string(LispParser parser)
+{
+
+}
+
+void lispparser_add_to_string(LispParser parser, unsigned char character)
+{
+
+}
+
+void lispparser_add_escaped_character(LispParser parser, unsigned char character)
+{
+    if (character == 'n') {
+        lispparser_add_to_string(parser, '\n');
+    } else if (character == '\\') {
+        lispparser_add_to_string(parser, '\\');
+    } else if (character == 't') {
+        lispparser_add_to_string(parser, '\t');
+    } else if (character == '"') {
+        lispparser_add_to_string(parser, '"');
+    } else {
+        lispparser_add_to_string(parser, character);
     }
-    char * result = calloc(symbol_length + 1, 1);
-    assert((unsigned long long int) result % 2 == 0);
-    strncpy(result, symbol, symbol_length);
-    result[symbol_length] = '\0';
-    parser->strings = lisppair_new(result, parser->strings);
-    return (void *) ((unsigned long long int) result | 1);
+    parser->escaping = 0;
 }
 
-void lispparser_append_item(LispParser parser, void * item)
+void lispparser_begin_string(LispParser parser)
 {
-    (*(parser->next_list)) = lisppair_new(item, NULL);
-    parser->next_list = &(parser->next_list->next);
+
 }
 
-void lispparser_add_new_list(LispParser parser)
+void lispparser_begin_list(LispParser parser)
 {
-    LispPair old_next_list = parser->next_list;
-    lispparser_append_item(parser, lisppair_new(NULL, NULL))
-    parser->parent = lisppair_new(parser->next_list, parser->parent);
+
 }
 
-void lispparser_current_list_ends(LispParser parser)
+void lispparser_end_list(LispParser parser)
 {
-    if (parser->parent == NULL) {
-        parser->error = 1;
-        return;
-    }
-    LispPair old_parent = parser->parent;
-    parser->parent = parser->parent->next;
-    parser->next_item = &(parser->parent->data);
-    free(old_parent);
+
 }
 
 void lispparser_handle_space(LispParser parser)
 {
-    if (parser->current_state == LISP_PARSER_IN_SYMBOL) {
-        parser->current_state = LISP_PARSER_BETWEEN_ATOMS;
-        parser->buffer[parser->symbol_length] = '\0';
-        *(parser->next_item) = lispparser_create_object(parser, parser->buffer, parser->symbol_length);
-        parser->next_item = &(parser->parent->data);
-        parser->symbol_length = 0;
-    }
+
 }
 
-void lispparser_handle_handle_char(LispParser parser, unsigned char character)
+void lispparser_handle_atom_char(LispParser parser, unsigned char character)
 {
-    if (parser->current_state == LISP_PARSER_BETWEEN_ATOMS) {
-        parser->current_state = LISP_PARSER_IN_SYMBOL;
-    }
 
-    parser->buffer[parser->symbol_length++] = character;
-    if (parser->symbol_length >= parser->buffer_size) {
-        parser->buffer_size += 64;
-        parser->buffer = realloc(parser->buffer, parser->buffer_size);
-    }
 }
 
-void lispparser_take_char(LispParser parser, int character)
+void lispparser_take_char(LispParser parser, unsigned char character)
 {
+    // For every character we process we keep tract of the line number and
+    // column we're up to.If we encounter a syntax error, the parser knows
+    // where it was.
     if (character == '\n') {
         parser->line_number++;
-        parser->character_number = 0;
+        parser->column_number = 0;
+    } else if (character == '\r') {
+
     } else {
-        parser->character_number++;
+        parser.column_number++;
     }
 
-    if (character == '(') {
-        lispparser_add_new_list(parser);
-    } else if (character == ')') {
-        lispparser_current_list_ends(parser);
-    } else if (isspace(character)) {
-        lispparser_handle_space(parser);
+    if (parser->in_string) {
+        if (parser->escaping) {
+            lispparser_add_escaped_character(parser, character);
+        } else {
+            if (character == '\\') {
+                lispparser_start_escaping(parser);
+            } else if (character == '"') {
+                lispparser_close_string(parser);
+            } else {
+                lispparser_add_to_string(parser, character);
+            }
+        }
     } else {
-        lispparser_handle_handle_char(parser, (unsigned char) character);
+        if (parser->escaping) {
+            lispparser->error = 1;
+            lispparser->error_msg = "The parser is escaping outside a string literal";
+            return;
+        } else if (character == '"') {
+            lispparser_begin_string(parser);
+        } else if (character == '(') {
+            lispparser_begin_list(parser);
+        } else if (character == ')') {
+            lispparser_end_list(parser);
+        } else if (isspace(character)) {
+            lispparser_handle_space(parser);
+        } else {
+            lispparser_handle_atom_char(parser, character);
+        }
     }
 }
 
-LispPair lispparser_get_result(LispParser parser)
+void lisppair_print(LispPair pair)
 {
-    return parser->root;
+
+}
+
+LispPair lispparser_get_root(LispParser parser)
+{
+    if (parser->result != NULL) {
+        return parser->result;
+    }
+
+    parser->result = calloc(sizeof(struct _LispPair), parser->parsed_tree_size);
+    unsigned int i = 0;
+    unsigned int next_element = 0;
+    while (i < parser->parsed_tree_size)
+    {
+        if (parser->parsed_tree[i].type == LISPPAIR_LIST) {
+            // the length of lists is stored in the value.as_int field
+            parser->result[next_element].type = LISPPAIR_LIST;
+            parser->result[next_element].length = parser->parsed_tree[i].value.as_int;
+            // We set the pointer to the first element of the list to null untill we load it.
+            parser->result[next_element].value.as_list = NULL;
+        } else {
+        }
+        i = parser->parsed_tree[i].next;
+    }
+    return parser->result;
 }
 
 void lispparser_delete(LispParser parser)
@@ -150,56 +216,17 @@ void lispparser_delete(LispParser parser)
 
 }
 
-void * lisppair_get_data(LispPair pair)
-{
-    return (void*) (((unsigned long long int)pair->data >> 1) << 1);
-}
-
-int lisppair_data_is_pair(LispPair obj)
-{
-    return !((unsigned long long int) obj->data & 1);
-}
-
-void lisppair_print(LispPair obj);
-
-void lisppair_print_remaining(LispPair obj)
-{
-    if (obj == NULL) {
-        printf(")");
-        return;
-    } else {
-        printf(" ");
-    }
-
-    if (lisppair_data_is_pair(obj)) {
-        lisppair_print(lisppair_get_data(obj));
-    } else {
-        printf("%s", lisppair_get_data(obj));
-    }
-    lisppair_print_remaining(obj->next);
-}
-
-void lisppair_print(LispPair obj)
-{
-    printf("(");
-    if (lisppair_data_is_pair(obj)) {
-        lisppair_print(obj->data);
-    } else {
-        printf("%s", lisppair_get_data(obj));
-    }
-    lisppair_print_remaining(obj->next);
-}
-
 int main()
 {
-    FILE * fhandle = fopen("test_lisp.txt", "r");
+    FILE * handle = fopen("test_lisp.txt", "r");
     LispParser parser = lispparser_new();
-    while (!feof(fhandle)) {
-        lispparser_take_char(parser, fgetc(fhandle));
+    while (!feof(handle) && !parser->error) {
+        lispparser_take_char(parser, fgetc(handle));
     }
-    fclose(fhandle);
-    LispPair result = lispparser_get_result(parser);
-    lisppair_print(result);
+    if (parser->error) {
+        printf("There was a parser error: %s\n", parser->error_msg);
+        return 1;
+    }
+    lisppair_print(lispparser_get_root(parser));
     lispparser_delete(parser);
 }
-
